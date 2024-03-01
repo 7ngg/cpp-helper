7c20abf131eeddc4bfd8f414a658b98f820cdf66
tng <m.farm4nov@gmail.com>
2024-03-01 10:33:30 -0800

    dynamic structures 3

---
+++ bdu-hw/dynamic_structures_3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void printArr(int (&arr)[], const int size) {
+    std::cout << "Array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int size;
+    std::cout << "Enter array size: ";
+    std::cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        printf_s("%d: ", i);
+        std::cin >> arr[i];
+    }
+    
+    printArr(arr, size);
+
+    int last = arr[size - 1];
+    for (int i = size - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = last;
+
+    printArr(arr, size);
+
+    return 0;
+}
